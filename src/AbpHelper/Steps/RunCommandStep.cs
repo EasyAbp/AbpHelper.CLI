97abdcd9ce4c3b1d8c4b871da992570deb646138
wakuflair <130427427@qq.com>
2020-02-05 13:23:58 +0800

    Implement RunCommandStep

--- src/AbpHelper/Steps/RunCommandStep.cs
+++ src/AbpHelper/Steps/RunCommandStep.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using AbpHelper.Utils;
+using Microsoft.Extensions.Logging;
 
 namespace AbpHelper.Steps
 {
@@ -11,9 +13,76 @@ namespace AbpHelper.Steps
         protected override Task RunStep()
         {
             LogInput(() => Command);
-            var output = CmdHelper.RunCmdAndGetOutput(Command);
-            Console.WriteLine(output);
+            var exitCode = RunCommand(Command);
+            if (exitCode != 0) throw new RunningCommandFailedException(exitCode);
+
             return Task.CompletedTask;
         }
+
+        private int RunCommand(string command)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo = new ProcessStartInfo(GetFileName())
+                {
+                    Arguments = GetArguments(command),
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                };
+                process.OutputDataReceived += (sender, args) =>
+                {
+                    if (!args.Data.IsNullOrEmpty()) Logger.LogDebug(args.Data);
+                };
+                process.ErrorDataReceived += (sender, args) =>
+                {
+                    if (!args.Data.IsNullOrEmpty()) Logger.LogError(args.Data);
+                };
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+                return process.ExitCode;
+            }
+        }
+
+        /// <summary>
+        ///     Copied from ABP CLI source
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public static string GetArguments(string command)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return "-c \"" + command + "\"";
+
+            //Windows default.
+            return "/C \"" + command + "\"";
+        }
+
+        /// <summary>
+        ///     Copied from ABP CLI source
+        /// </summary>
+        /// <returns></returns>
+        public static string GetFileName()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                //TODO: Test this. it should work for both operation systems.
+                return "/bin/bash";
+
+            //Windows default.
+            return "cmd.exe";
+        }
+    }
+
+    public class RunningCommandFailedException : Exception
+    {
+        public RunningCommandFailedException(int exitCode)
+        {
+            ExitCode = exitCode;
+        }
+
+        public int ExitCode { get; }
     }
 }
--- src/AbpHelper/Workflow/Abp/MigrationAndUpdateDatabaseWorkflow.cs
+++ src/AbpHelper/Workflow/Abp/MigrationAndUpdateDatabaseWorkflow.cs
@@ -9,7 +9,6 @@ namespace AbpHelper.Workflow.Abp
         public static WorkflowBuilder AddMigrationAndUpdateDatabaseWorkflow(this WorkflowBuilder builder)
         {
             return builder
-                    /* Add migration */
                     .AddStep<FileFinderStep>(
                         step => step.SearchFileName = "*.EntityFrameworkCore.DbMigrations.csproj",
                         step => step.ResultParameterName = "MigrationProjectFile"
@@ -18,13 +17,22 @@ namespace AbpHelper.Workflow.Abp
                         step => step.SearchFileName = "*.Web.csproj",
                         step => step.ResultParameterName = "WebProjectFile"
                     )
+                    /* Add migration */
                     .AddStep<RunCommandStep>(
                         step =>
                         {
                             var entityInfo = step.Get<EntityInfo>();
                             var migrationProjectFile = step.GetParameter<string>("MigrationProjectFile");
                             var webProjectFile = step.GetParameter<string>("WebProjectFile");
-                            step.Command = $"dotnet ef migrations add Add{entityInfo.ClassName} -p \"{migrationProjectFile}\" -s \"{webProjectFile}\"";
+                            step.Command = $"dotnet ef migrations add Added{entityInfo.ClassName} -p \"{migrationProjectFile}\" -s \"{webProjectFile}\"";
+                        })
+                    /* Update database */
+                    .AddStep<RunCommandStep>(
+                        step =>
+                        {
+                            var migrationProjectFile = step.GetParameter<string>("MigrationProjectFile");
+                            var webProjectFile = step.GetParameter<string>("WebProjectFile");
+                            step.Command = $"dotnet ef database update -p \"{migrationProjectFile}\" -s \"{webProjectFile}\"";
                         })
                 ;
         }
