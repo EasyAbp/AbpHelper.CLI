31b9dee59f8b20223eeaa58872fd11f0c5934a63
wakuflair <130427427@qq.com>
2020-02-05 16:27:02 +0800

    Implement DirectoryFinderStep

---
+++ src/AbpHelper/Steps/DirectoryFinderStep.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbpHelper.Steps
+{
+    public class DirectoryFinderStep : Step
+    {
+        public string SearchDirectoryName { get; set; } = string.Empty;
+        public string ResultParameterName { get; set; } = "DirectoryPathName";
+
+        protected override Task RunStep()
+        {
+            var baseDirectory = GetParameter<string>("BaseDirectory");
+            LogInput(() => baseDirectory);
+            LogInput(() => SearchDirectoryName);
+
+            var directoryPathName = Directory.EnumerateDirectories(baseDirectory, SearchDirectoryName, SearchOption.AllDirectories).Single();
+            SetParameter(ResultParameterName, directoryPathName);
+            LogOutput(() => directoryPathName, $"Found directory: {directoryPathName}, stored in parameter: [{ResultParameterName}]");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/AbpHelper/Steps/FileGenerationStep.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace AbpHelper.Steps
+{
+    public class FileGenerationStep : Step
+    {
+        public string File { get; set; } = string.Empty;
+        public string Contents { get; set; } = string.Empty;
+
+        protected override async Task RunStep()
+        {
+            var targetFile = File.IsNullOrEmpty() ? GetParameter<string>("FilePathName") : File;
+
+            LogInput(() => targetFile);
+            LogInput(() => Contents, $"Contents length: {Contents.Length}");
+
+            var dir = Path.GetDirectoryName(targetFile);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+                Logger.LogInformation($"Directory {dir} created.");
+            }
+
+            await System.IO.File.WriteAllTextAsync(targetFile, Contents);
+            Logger.LogInformation($"File {targetFile} generated.");
+        }
+    }
+}
