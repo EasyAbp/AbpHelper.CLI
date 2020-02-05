bd3fb4ca22320a90fe439eb46ebc594df04498a8
wakuflair <130427427@qq.com>
2020-02-05 10:03:04 +0800

    Implement RunCommandStep

---
+++ src/AbpHelper/Steps/RunCommandStep.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using AbpHelper.Utils;
+
+namespace AbpHelper.Steps
+{
+    public class RunCommandStep : Step
+    {
+        public string Command { get; set; } = string.Empty;
+
+        protected override Task RunStep()
+        {
+            LogInput(() => Command);
+            var output = CmdHelper.RunCmdAndGetOutput(Command);
+            Console.WriteLine(output);
+            return Task.CompletedTask;
+        }
+    }
+}
