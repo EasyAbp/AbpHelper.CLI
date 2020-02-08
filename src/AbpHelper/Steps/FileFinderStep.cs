26b6d6a5789cfd2d097920d809d57a6fad240d37
wakuflair <130427427@qq.com>
2020-02-08 15:29:21 +0800

    Support for multi file finding

--- src/AbpHelper/Steps/FileFinderStep.cs
+++ src/AbpHelper/Steps/FileFinderStep.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -6,19 +7,35 @@ namespace AbpHelper.Steps
 {
     public class FileFinderStep : Step
     {
+        public const string DefaultFilesParameterName = "FileFinderResult";
+        public string BaseDirectory { get; set; } = string.Empty;
         public string SearchFileName { get; set; } = string.Empty;
-        public string ResultParameterName { get; set; } = "FilePathName";
+        public string ResultParameterName { get; set; } = DefaultFilesParameterName;
+        public bool Multiple { get; set; } = false;
 
         protected override Task RunStep()
         {
-            var baseDirectory = GetParameter<string>("BaseDirectory");
+            var baseDirectory = BaseDirectory.IsNullOrEmpty() ? GetParameter<string>("BaseDirectory") : BaseDirectory;
             LogInput(() => baseDirectory);
             LogInput(() => SearchFileName);
+            LogInput(() => Multiple);
 
-            var filePathName = Directory.EnumerateFiles(baseDirectory, SearchFileName, SearchOption.AllDirectories).SingleOrDefault();
-            if (filePathName == null) throw new FileNotFoundException();
-            SetParameter(ResultParameterName, filePathName);
-            LogOutput(() => filePathName, $"Found file: {filePathName}, stored in parameter: [{ResultParameterName}]");
+            var files = Directory.EnumerateFiles(baseDirectory, SearchFileName, SearchOption.AllDirectories).ToArray();
+
+            if (Multiple)
+            {
+                if (files.Length == 0) throw new FileNotFoundException();
+
+                SetParameter(ResultParameterName, files);
+                LogOutput(() => files, $"Found files count: {files.Length}, stored in parameter: [{ResultParameterName}]");
+            }
+            else
+            {
+                var filePathName = files.SingleOrDefault();
+                if (filePathName == null) throw new FileNotFoundException();
+                SetParameter(ResultParameterName, filePathName);
+                LogOutput(() => filePathName, $"Found file: {filePathName}, stored in parameter: [{ResultParameterName}]");
+            }
 
             return Task.CompletedTask;
         }
