36eda3ac4c3fb43d58abd1d953f4c69310ebea2d
Enver Francisco <enver.francisco@gmail.com>
2020-06-20 01:26:47 -0400

    adding missing files to the PR

--- src/AbpHelper/Templates/Crud/Groups/UiRazor/src/{{ProjectInfo.FullName}}.Web/Pages/{{Bag.PagesFolder}}/{{EntityInfo.RelativeDirectory}}/{{EntityInfo.Name}}/ViewModels/Create{{EntityInfo.Name}}ViewModel.cs
+++ src/AbpHelper/Templates/Crud/Groups/UiRazor/src/{{ProjectInfo.FullName}}.Web/Pages/{{Bag.PagesFolder}}/{{EntityInfo.RelativeDirectory}}/{{EntityInfo.Name}}/ViewModels/Create{{EntityInfo.Name}}ViewModel.cs
@@ -15,7 +15,22 @@ namespace {{ ProjectInfo.FullName }}.Web.Pages.{{ pagesNamespace }}{{ EntityInfo
         [Display(Name = "{{ EntityInfo.Name + prop.Name}}")]
         {{~ end ~}}
         {{~ if string.ends_with prop.Type ">" ~}} 
-        public  {{ prop.Type | string.replace "<" "<Create" | string.replace ">" "ViewModel>"}} Create{{ prop.Name }}ViewModel { get; set; }
+        public {{
+        stRgex = prop.Name | regex.replace "s" "." "$"
+        stStart = "<" | string.append stRgex
+        stFinal = stStart | string.append "ViewModels."
+        stFinalEx = stFinal | string.append "Create"
+        stRgexTwo = prop.Name | regex.replace "s" "ViewModel" "$"
+        stFinalNoEx = stFinalEx | string.append stRgexTwo
+        stAppend = stFinalNoEx | string.append ">"
+        stRegexFind = prop.Name | regex.replace "s" ">" "$"
+
+        stFind = "<" | string.append stRegexFind
+
+
+        prop.Type | string.replace stFind stAppend
+
+        }} Create{{ prop.Name }}ViewModel { get; set; }
         {{~ else ~}}
         public {{ prop.Type}} {{ prop.Name }} { get; set; }
         {{~end ~}}
--- src/AbpHelper/Templates/Crud/Groups/UiRazor/src/{{ProjectInfo.FullName}}.Web/Pages/{{Bag.PagesFolder}}/{{EntityInfo.RelativeDirectory}}/{{EntityInfo.Name}}/ViewModels/Edit{{EntityInfo.Name}}ViewModel.cs
+++ src/AbpHelper/Templates/Crud/Groups/UiRazor/src/{{ProjectInfo.FullName}}.Web/Pages/{{Bag.PagesFolder}}/{{EntityInfo.RelativeDirectory}}/{{EntityInfo.Name}}/ViewModels/Edit{{EntityInfo.Name}}ViewModel.cs
@@ -15,7 +15,22 @@ namespace {{ ProjectInfo.FullName }}.Web.Pages.{{ pagesNamespace }}{{ EntityInfo
         [Display(Name = "{{ EntityInfo.Name + prop.Name}}")]
         {{~ end ~}}
         {{~ if string.ends_with prop.Type ">" ~}}     
-        public {{ prop.Type | string.replace "<" "<Edit" | string.replace ">" "ViewModel>"}} Edit{{ prop.Name }}ViewModel { get; set; }
+        public {{
+        stRgex = prop.Name | regex.replace "s" "." "$"
+        stStart = "<" | string.append stRgex
+        stFinal = stStart | string.append "ViewModels."
+        stFinalEx = stFinal | string.append "Edit"
+        stRgexTwo = prop.Name | regex.replace "s" "ViewModel" "$"
+        stFinalNoEx = stFinalEx | string.append stRgexTwo
+        stAppend = stFinalNoEx | string.append ">"
+        stRegexFind = prop.Name | regex.replace "s" ">" "$"
+
+        stFind = "<" | string.append stRegexFind
+
+
+        prop.Type | string.replace stFind stAppend
+
+        }} Edit{{ prop.Name }}ViewModel { get; set; }
         {{~ else ~}}
         public {{ prop.Type}} {{ prop.Name }} { get; set; }
         {{~end ~}}
