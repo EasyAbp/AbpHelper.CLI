196ee7998297b156ac865491a425b4165f9ca153
wakuflair <wakuflair@163.com>
2020-08-01 16:13:52 +0800

    Add default value for `ExcludeDirectories`

--- src/AbpHelper/Commands/CommandOptionsBase.cs
+++ src/AbpHelper/Commands/CommandOptionsBase.cs
@@ -1,4 +1,5 @@
-﻿using EasyAbp.AbpHelper.Attributes;
+﻿using System;
+using EasyAbp.AbpHelper.Attributes;
 
 namespace EasyAbp.AbpHelper.Commands
 {
@@ -8,7 +9,7 @@ namespace EasyAbp.AbpHelper.Commands
         public virtual string Directory { get; set; } = null!;
 
         [Option("exclude", Description = "Exclude directories when searching files, arguments can contain a combination of valid literal path and wildcard (* and ?) characters. Use double asterisk(**) to search all directories. Example: --exclude *Folder1,Folder2/Folder*,**/*Folder?,**/*Folder*")]
-        public virtual string[] ExcludeDirectories { get; set; } = null!;
+        public virtual string[] ExcludeDirectories { get; set; } = Array.Empty<string>();
 
         [Option("no-overwrite", Description = "Specify not to overwrite existing files or content")]
         public bool NoOverwrite { get; set; }
