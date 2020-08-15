12699affc6db5cde332a849dacf19b9b433a9c00
wakuflair <wakuflair@163.com>
2020-08-15 15:18:50 +0800

    Extract `AddConfigureMigrationProjectsWorkflow`

---
+++ src/AbpHelper/Steps/Common/ToActivityBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Elsa.Results;
+using Elsa.Services.Models;
+
+namespace EasyAbp.AbpHelper.Steps.Common
+{
+    /// <summary>
+    /// This is an empty step.
+    /// It can convert `IOutcomeBuilder` to `IActivityBuilder` when writing fluent steps in a workflow,
+    /// which is useful when writing extension workflow methods
+    /// </summary>
+    public class ToActivityBuilder : Step
+    {
+        protected override ActivityExecutionResult OnExecute(WorkflowExecutionContext context)
+        {
+            return Done();
+        }
+    }
+}
--- src/AbpHelper/Workflow/Generate/Crud/MigrationAndUpdateDatabaseWorkflow.cs
+++ src/AbpHelper/Workflow/Generate/Crud/MigrationAndUpdateDatabaseWorkflow.cs
@@ -1,11 +1,5 @@
-﻿using EasyAbp.AbpHelper.Commands;
-using EasyAbp.AbpHelper.Commands.Generate.Crud;
-using EasyAbp.AbpHelper.Models;
-using EasyAbp.AbpHelper.Steps.Common;
-using Elsa;
-using Elsa.Activities;
-using Elsa.Activities.ControlFlow.Activities;
-using Elsa.Expressions;
+﻿using EasyAbp.AbpHelper.Steps.Common;
+using EasyAbp.AbpHelper.Workflow.Common;
 using Elsa.Scripting.JavaScript;
 using Elsa.Services;
 
@@ -13,118 +7,11 @@ namespace EasyAbp.AbpHelper.Workflow.Generate.Crud
 {
     public static class MigrationAndUpdateDatabaseWorkflow
     {
-        private const string StartupProjectFile = nameof(StartupProjectFile);
-        private const string MigrationProjectFile = nameof(MigrationProjectFile);
-        private const string ModuleMigrationProjectName = nameof(ModuleMigrationProjectName);
-        private const string AppMigrationProjectName = nameof(AppMigrationProjectName);
-
         public static IActivityBuilder AddMigrationAndUpdateDatabaseWorkflow(this IOutcomeBuilder builder)
         {
             return builder
-                    .Then<IfElse>(
-                        ifElse => ifElse.ConditionExpression = new JavaScriptExpression<bool>($"{CommandConsts.OptionVariableName}.{nameof(CrudCommandOptions.MigrationProjectName)} == null"),
-                        ifElse =>
-                        {
-                            ifElse
-                                .When(OutcomeNames.True)
-                                .Then<SetVariable>(
-                                    step =>
-                                    {
-                                        step.VariableName = AppMigrationProjectName;
-                                        step.ValueExpression = new LiteralExpression("*.EntityFrameworkCore.DbMigrations.csproj");
-                                    })
-                                .Then<SetVariable>(
-                                    step =>
-                                    {
-                                        step.VariableName = ModuleMigrationProjectName;
-                                        step.ValueExpression = new LiteralExpression("*.HttpApi.Host.csproj");
-                                    })
-                                .Then(ActivityNames.SearchFiles)
-                                ;
-                            ifElse
-                                .When(OutcomeNames.False)
-                                .Then<SetVariable>(
-                                    step =>
-                                    {
-                                        step.VariableName = AppMigrationProjectName;
-                                        step.ValueExpression = new JavaScriptExpression<string>($"{CommandConsts.OptionVariableName}.{nameof(CrudCommandOptions.MigrationProjectName)}");
-                                    })
-                                .Then<SetVariable>(
-                                    step =>
-                                    {
-                                        step.VariableName = ModuleMigrationProjectName;
-                                        step.ValueExpression = new JavaScriptExpression<string>($"{CommandConsts.OptionVariableName}.{nameof(CrudCommandOptions.MigrationProjectName)}");
-                                    })
-                                .Then(ActivityNames.SearchFiles)
-                                ;
-                        }
-                    )
-                    .Then<IfElse>(
-                        ifElse => ifElse.ConditionExpression = new JavaScriptExpression<bool>
-                            ($"ProjectInfo.TemplateType == {TemplateType.Application:D}"),
-                        ifElse =>
-                        {
-                            // Application
-                            ifElse
-                                .When(OutcomeNames.True)
-                                .Then<FileFinderStep>(
-                                    step =>
-                                    {
-                                        step.SearchFileName = new JavaScriptExpression<string>(AppMigrationProjectName);
-                                        step.ResultVariableName = new LiteralExpression(MigrationProjectFile);
-                                    }
-                                )
-                                .Then<IfElse>(
-                                    ie => ie.ConditionExpression = new JavaScriptExpression<bool>
-                                        ($"ProjectInfo.UiFramework == {UiFramework.RazorPages:D}"),
-                                    ie =>
-                                    {
-                                        ie.When(OutcomeNames.True)
-                                            .Then<FileFinderStep>(
-                                                step =>
-                                                {
-                                                    step.SearchFileName = new LiteralExpression("*.Web.csproj");
-                                                    step.ResultVariableName = new LiteralExpression<string>(StartupProjectFile);
-                                                })
-                                            .Then(ActivityNames.RunMigration)
-                                            ;
-                                    })
-                                .Then<IfElse>(
-                                    ie => ie.ConditionExpression = new JavaScriptExpression<bool>
-                                        ($"ProjectInfo.UiFramework == {UiFramework.None:D}"),
-                                    ie =>
-                                    {
-                                        ie.When(OutcomeNames.True)
-                                            .Then<FileFinderStep>(
-                                                step =>
-                                                {
-                                                    step.SearchFileName = new LiteralExpression("*.DbMigrator.csproj");
-                                                    step.ResultVariableName = new LiteralExpression<string>(StartupProjectFile);
-                                                })
-                                            .Then(ActivityNames.RunMigration)
-                                            ;
-                                    });
-                            // Module
-                            ifElse
-                                .When(OutcomeNames.False)
-                                .Then<FileFinderStep>(
-                                    step =>
-                                    {
-                                        step.SearchFileName = new JavaScriptExpression<string>(ModuleMigrationProjectName);
-                                        step.ResultVariableName = new LiteralExpression(MigrationProjectFile);
-                                    }
-                                )
-                                .Then<FileFinderStep>(
-                                    step =>
-                                    {
-                                        step.SearchFileName = new JavaScriptExpression<string>(ModuleMigrationProjectName); // For module, the startup project is same with the migration project
-                                        step.ResultVariableName = new LiteralExpression<string>(StartupProjectFile);
-                                    }
-                                )
-                                .Then(ActivityNames.RunMigration)
-                                ;
-                        }
-                    ).WithName(ActivityNames.SearchFiles)
+                    .Then<ToActivityBuilder>()
+                    .AddConfigureMigrationProjectsWorkflow()
                     /* Add migration */
                     .Then<RunCommandStep>(
                         step => step.Command = new JavaScriptExpression<string>("`dotnet ef migrations add Added${EntityInfo.Name} -p \"${MigrationProjectFile}\" -s \"${StartupProjectFile}\"`")
