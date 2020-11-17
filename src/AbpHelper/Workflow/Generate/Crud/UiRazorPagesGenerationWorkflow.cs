d4297a14c131c801e47b02bfa4b38c0ea756a687
wakuflair <wakuflair@163.com>
2020-11-17 11:22:26 +0800

    fix https://github.com/EasyAbp/AbpHelper.GUI/issues/33

--- src/AbpHelper/Workflow/Generate/Crud/UiRazorPagesGenerationWorkflow.cs
+++ src/AbpHelper/Workflow/Generate/Crud/UiRazorPagesGenerationWorkflow.cs
@@ -1,9 +1,11 @@
-﻿using EasyAbp.AbpHelper.Steps.Abp;
+﻿using System.Collections.Generic;
+using EasyAbp.AbpHelper.Steps.Abp;
 using EasyAbp.AbpHelper.Steps.Abp.ModificationCreatorSteps.CSharp;
 using EasyAbp.AbpHelper.Steps.Common;
 using Elsa;
 using Elsa.Activities;
 using Elsa.Activities.ControlFlow.Activities;
+using Elsa.Expressions;
 using Elsa.Scripting.JavaScript;
 using Elsa.Services;
 
@@ -46,17 +48,35 @@ namespace EasyAbp.AbpHelper.Workflow.Generate.Crud
                         }
                     ).WithName(ActivityNames.UiRazor)
                     /* Add menu name */
-                    .Then<FileFinderStep>(
-                        step => step.SearchFileName = new JavaScriptExpression<string>("`${ProjectInfo.Name}Menus.cs`")
+                    .Then<MultiFileFinderStep>(
+                        step =>
+                        {
+                            step.SearchFileName = new JavaScriptExpression<string>("`${ProjectInfo.Name}Menus.cs`");
+                        }
                     )
-                    .Then<MenuNameStep>()
-                    .Then<FileModifierStep>() 
+                    .Then<ForEach>(
+                        x => { x.CollectionExpression = new JavaScriptExpression<IList<object>>(MultiFileFinderStep.DefaultFileParameterName); },
+                        branch =>
+                            branch.When(OutcomeNames.Iterate)
+                                .Then<MenuNameStep>(step => step.SourceFile = new JavaScriptExpression<string>("CurrentValue"))
+                                .Then<FileModifierStep>(step => step.TargetFile = new JavaScriptExpression<string>("CurrentValue"))
+                                .Then(branch)
+                    )
                     /* Add menu */
-                    .Then<FileFinderStep>(
-                        step => step.SearchFileName = new JavaScriptExpression<string>("`${ProjectInfo.Name}MenuContributor.cs`")
+                    .Then<MultiFileFinderStep>(
+                        step =>
+                        {
+                            step.SearchFileName = new JavaScriptExpression<string>("`${ProjectInfo.Name}MenuContributor.cs`");
+                        }
                     )
-                    .Then<MenuContributorStep>()
-                    .Then<FileModifierStep>()
+                    .Then<ForEach>(
+                        x => { x.CollectionExpression = new JavaScriptExpression<IList<object>>(MultiFileFinderStep.DefaultFileParameterName); },
+                        branch =>
+                            branch.When(OutcomeNames.Iterate)
+                                .Then<MenuContributorStep>(step => step.SourceFile = new JavaScriptExpression<string>("CurrentValue"))
+                                .Then<FileModifierStep>(step => step.TargetFile = new JavaScriptExpression<string>("CurrentValue"))
+                                .Then(branch)
+                    )
                     /* Add mapping */
                     .Then<FileFinderStep>(
                         step =>
