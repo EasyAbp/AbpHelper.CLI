bf8e6b37691101a7ab711a704fc7c61b9283f98a
wakuflair <wakuflair@163.com>
2021-02-22 15:22:31 +0800

    Implement ListPackageService

---
+++ src/AbpHelper.Core/Services/IListPackageService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EasyAbp.AbpHelper.Core.Models;
+
+namespace EasyAbp.AbpHelper.Core.Services
+{
+    public interface IListPackageService
+    {
+        Task<Dictionary<string, List<PackageInfo>>> GetInstalledPackagesAsync(string baseDirectory);
+    }
+}
---
+++ src/AbpHelper.Core/Services/ListPackageService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using EasyAbp.AbpHelper.Core.Models;
+
+namespace EasyAbp.AbpHelper.Core.Services
+{
+    public class ListPackageService : IListPackageService
+    {
+        public Task<Dictionary<string, List<PackageInfo>>> GetInstalledPackagesAsync(string baseDirectory)
+        {
+            var result = new Dictionary<string, List<PackageInfo>>();
+
+            var csprojFiles = Directory.EnumerateFiles(baseDirectory, "*.csproj");
+            foreach (var csprojFile in csprojFiles)
+            {
+                string projectName = Path.GetFileNameWithoutExtension(csprojFile);
+                result.Add(projectName, new List<PackageInfo>());
+
+                var root = XDocument.Load(csprojFile).Root;
+                foreach (var element in root.Descendants("PackageReference"))
+                {
+                    string name = element.Attribute("Include")!.Value;
+                    string version = element.Attribute("Version")!.Value;
+                    result[projectName].Add(new PackageInfo(name, version));
+                }
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
